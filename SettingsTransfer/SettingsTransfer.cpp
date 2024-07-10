88483b0f1d435054ecb50d9bad7a331fd5d79858
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-07-10 11:18:45 -0700

    outputs file path without crashing

--- SettingsTransfer/SettingsTransfer.cpp
+++ SettingsTransfer/SettingsTransfer.cpp
@@ -1,8 +1,10 @@
 #include <gtk/gtk.h>
 #include <windows.h>
 #include <string>
+#include <iostream>
 
 int counter{1};
+std::string file1;
 struct File1 {
     std::string selected_file1;
 };
@@ -12,15 +14,43 @@ struct File2 {
 };
 
 void open_file_dialog(GObject* source_object, GAsyncResult *res, gpointer user_data) {
+
     g_print("file successfully selected\n");
+
+
     if(::counter == 1) {
-        File1* data = static_cast<File1*>(user_data);
-        g_print("File1: %s\n", data);
-    }
+        GError* error = NULL;
+        GFile* file = gtk_file_dialog_open_finish(GTK_FILE_DIALOG(source_object), res, &error);
+        g_print("it at least gets here\n");
+
+        if(error) {
+            g_print("Error opening file: %s\n", error->message);
+            g_error_free(error);
+        }
+        if (file) {
+            g_print("checkpoint 3\n");
+            char* path = g_file_get_path(file);
+
+            g_print("checkpoint 3.5\n");
+            if (path) {
+                g_print("checkpiont 4\n");
+                ::file1 = std::string(path);
+                g_print("checkpoint 4.5\n");
+                g_free(path);
+                g_print("checkpoint 4.7");
+                g_print("Selected file: %s\n", path);
+                g_print("string version%s\n", ::file1);
+                ::counter++;
+            }
+            g_object_unref(file);  // Clean up the GFile object
+        } else {
+            g_print("No file selected.\n");
+        }
     
+    }
 }
 
-static void onButtonPress(GtkWidget *widget, gpointer user_data){
+static void onButtonPress(GtkWidget *widget, gpointer user_data) {
     //static std::string file1;
     //static std::string file2;
 
@@ -31,7 +61,7 @@ static void onButtonPress(GtkWidget *widget, gpointer user_data){
     g_print("Hello World\n");
 
     if(::counter == 1) {
-        g_print("option 1");
+        g_print("option 1\n");
         gtk_file_dialog_open(dialog, window, NULL, open_file_dialog, user_data);
     }
 
@@ -42,9 +72,6 @@ static void onButtonPress(GtkWidget *widget, gpointer user_data){
     if(::counter == 3) {
 
     }
-
-    
-    ::counter++;
 }
 
 /* there will be no button updating until I figure out a way to detect a resize that doesn't crash the program
@@ -89,7 +116,7 @@ static void activate(GtkApplication *app, gpointer user_data){
     fixed = gtk_fixed_new();
     gtk_window_set_child(GTK_WINDOW(window), fixed);
 
-    button = gtk_button_new_with_label("Hello Bitch");
+    button = gtk_button_new_with_label("Select File");
     gtk_fixed_put(GTK_FIXED(fixed), button, buttonX, buttonY);
     gtk_widget_set_size_request(button, buttonWidth, buttonHeight);    
     
