b793c42bc738bf1cb59fa275cbde9e7064577660
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-07-21 22:47:51 -0700

    added debug toggle

--- SettingsTransfer/SettingsTransfer.cpp
+++ SettingsTransfer/SettingsTransfer.cpp
@@ -2,21 +2,25 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+
+bool DEBUG = true;
+
 /*this is the version of SettingsTransfer that attempts to use a struct instead of a class*/
-std::string PROMPT1 = "To get started, select the \"options.txt\" file of the old minecraft version you \
+const char *PROMPT1 = "To get started, select the \"options.txt\" file of the old minecraft version you \
     want to transfer the settings from.";
 
-std::string PROMPT2 = "Next, select the \"options.txt\" file of the new minecraft version you want to \
+const char *PROMPT2 = "Next, select the \"options.txt\" file of the new minecraft version you want to \
     transfer the settings to. Make sure you have already installed the mods you want, and you have launced this\
     version at least once. ";
 
-std::string PROMPT3 = "Transfer Settings!";
+const char *PROMPT3 = "Transfer Settings!";
 
 
 struct appdata{
     GtkApplication* app;
-    GtkWidget *window, *nextButton, *fixed, *backButton; 
+    GtkWidget *window, *nextButton, *fixed, *backButton, *view; 
     GtkFileDialog *fileDialog;
+    GtkTextBuffer *buffer;
 
     int counter{1}, screenWidth, screenHeight, nextButtonWidth, nextButtonHeight;
     double nextButtonX, nextButtonY;
@@ -37,27 +41,27 @@ void open_file_dialog(GObject* source_object, GAsyncResult *res, gpointer user_d
     //why didnt I do this in python?
     appdata *AppData = static_cast<appdata*>(user_data);
 
-    g_print("file successfully selected\n");
+    if(::DEBUG) g_print("file successfully selected\n");
 
     GError* error = NULL;
     GFile* file = gtk_file_dialog_open_finish(GTK_FILE_DIALOG(AppData->fileDialog), res, &error);
-    g_print("it at least gets here\n");
+    if(::DEBUG) g_print("it at least gets here\n");
 
     if(error) {
-        g_print("Error opening file: %s\n", error->message);
+        if(::DEBUG) g_print("Error opening file: %s\n", error->message);
         g_error_free(error);
     }
     if (file) {
-        g_print("checkpoint 3\n");
+        if(::DEBUG) g_print("checkpoint 3\n");
         char* path = g_file_get_path(file);
-        g_print("checkpoint 3.5\n");
+        if(::DEBUG) g_print("checkpoint 3.5\n");
 
         if(AppData->counter == 1) { 
             if (path) {
                 AppData->file1 = std::string(path);
                 g_free(path);
-                g_print("File 1: %s\n", path);
-                std::cout << "String Version: " << AppData->file1 << std::endl;
+                if(::DEBUG) g_print("File 1: %s\n", path);
+                if(::DEBUG) std::cout << "String Version: " << AppData->file1 << std::endl;
 
                 if(AppData->file1.ends_with("options.txt")){
                     AppData->counter++;                    
@@ -70,8 +74,8 @@ void open_file_dialog(GObject* source_object, GAsyncResult *res, gpointer user_d
             if (path) {
                 AppData->file2 = std::string(path);
                 g_free(path);
-                g_print("File 2: %s\n", path);
-                std::cout << "String Version: " << AppData->file2 << std::endl;
+                if(::DEBUG) g_print("File 2: %s\n", path);
+                if(::DEBUG) std::cout << "String Version: " << AppData->file2 << std::endl;
 
                 if(AppData->file2.ends_with("options.txt")){
                     AppData->counter++;                    
@@ -83,7 +87,7 @@ void open_file_dialog(GObject* source_object, GAsyncResult *res, gpointer user_d
 
         } 
             
-        } else {g_print("No file selected.\n");}
+        } else {if(::DEBUG) g_print("No file selected.\n");}
               
         // Clean up the GFile object
         g_object_unref(file);
@@ -97,15 +101,15 @@ static void onButtonPress(GtkWidget *widget, gpointer user_data) {
     AppData->fileDialog = gtk_file_dialog_new();
     
 
-    g_print("Hello World\n");
+    if(::DEBUG) g_print("Hello World\n");
 
     if(AppData->counter == 1) {
-        g_print("option 1\n");
+        if(::DEBUG) g_print("option 1\n");
         gtk_file_dialog_open(AppData->fileDialog, GTK_WINDOW(AppData->window), NULL, open_file_dialog, user_data);
     }
 
     if(AppData->counter == 2) {
-        g_print("option 2\n");       
+        if(::DEBUG) g_print("option 2\n");       
         gtk_file_dialog_open(AppData->fileDialog, GTK_WINDOW(AppData->window), NULL, open_file_dialog, user_data);
     }
 
@@ -149,7 +153,7 @@ static void activate(GtkApplication *app, gpointer user_data){
 
     AppData->screenWidth = GetSystemMetrics(SM_CXSCREEN) / sqrt(2);
     AppData->screenHeight = GetSystemMetrics(SM_CYSCREEN) / sqrt(2);
-    std::cout << "things initialized" << "\nScreenWidth: " << AppData->screenWidth << std::endl;
+    if(::DEBUG) std::cout << "things initialized" << "\nScreenWidth: " << AppData->screenWidth << std::endl;
     AppData->nextButtonWidth = AppData->screenWidth / 10;
     AppData->nextButtonHeight = AppData->screenHeight / 10;
     AppData->nextButtonX = (AppData->screenWidth * .5) - (.5 * AppData->nextButtonWidth);
@@ -163,15 +167,21 @@ static void activate(GtkApplication *app, gpointer user_data){
     AppData->fixed = gtk_fixed_new();
     gtk_window_set_child(GTK_WINDOW(AppData->window), AppData->fixed);
 
+   /* if(::DEBUG) g_print("before textview\n");
+    AppData->view = gtk_text_view_new();
+    AppData->buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(AppData->view));
+    gtk_text_buffer_set_text(AppData->buffer, ::PROMPT1, -1);
+    gtk_window_set_child(GTK_WINDOW(AppData->window), AppData->view);*/
+
+    if(::DEBUG) g_print("before next button\n");
     AppData->nextButton = gtk_button_new_with_label("Select File");
     updateButtons(AppData);    
     
     g_signal_connect(AppData->nextButton, "clicked", G_CALLBACK(onButtonPress), user_data);
 
+    if(::DEBUG) g_print("before presenting window\n");
     gtk_window_present(GTK_WINDOW(AppData->window));
- 
-    AppData->file1 = "fuck you";
-}
+ }
 
 
 int main (int argc, char **argv){
@@ -179,14 +189,14 @@ int main (int argc, char **argv){
     static appdata AppData;
     appdata *AppDataPtr = &AppData;
 
-    g_print("after defining struct\n");
+    if(::DEBUG) g_print("after defining struct\n");
     int status;
-    g_print("before new app\n");
+    if(::DEBUG) g_print("before new app\n");
     
     AppData.app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_print("new app successful\n");
+    if(::DEBUG) g_print("new app successful\n");
     g_signal_connect(AppData.app, "activate", G_CALLBACK (activate), AppDataPtr);
-    g_print("main runs\n");
+    if(::DEBUG) g_print("main runs\n");
 
     status = g_application_run (G_APPLICATION(AppData.app), argc, argv);
     g_object_unref(AppData.app);
